b53193308481a526edadbdf6cc6b86afe9731686
Todd Schiller <todd.schiller@gmail.com>
2013-01-14 10:36:22 -0800

    enforce checkstyle rules in mapviewer plugin

--- edu.washington.cs.cupid.mapview/src/edu/washington/cs/cupid/mapview/internal/GraphNodeContentProvider.java
+++ edu.washington.cs.cupid.mapview/src/edu/washington/cs/cupid/mapview/internal/GraphNodeContentProvider.java
@@ -1,6 +1,5 @@
 package edu.washington.cs.cupid.mapview.internal;
 
-import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
@@ -12,51 +11,55 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
 
 /**
- * Graph content provider for maps. If all values are sets, the elements of the sets are
- * displayed as individual elements
+ * Graph content provider for maps. If all values are {@link Set}s, the elements of the sets are
+ * displayed as individual elements.
  * @author Todd Schiller
  */
 public class GraphNodeContentProvider extends ArrayContentProvider implements IGraphEntityContentProvider {
 
-	private final Map<?,?> map;
+	private final Map<?, ?> map;
 	private final boolean mappedToSets;
 	
-	public GraphNodeContentProvider(Map<?,?> map){
+	/**
+	 * Construct a graph content provider for <code>map</code>.
+	 * @param map the underlying map
+	 */
+	public GraphNodeContentProvider(final Map<?, ?> map) {
 		this.map = map;
 		
-		this.mappedToSets = Iterables.all(map.values(), new Predicate<Object>(){
+		this.mappedToSets = Iterables.all(map.values(), new Predicate<Object>() {
 			@Override
-			public boolean apply(Object value) {
+			public boolean apply(final Object value) {
 				return value instanceof Set;
 			}
 		});
 	}
 
 	@Override
-	public Object[] getConnectedTo(Object entity) {
-		if (map.get(entity) != null){
-			if (mappedToSets){
-				return ((Set<?>)map.get(entity)).toArray();
-			}else{
+	public final Object[] getConnectedTo(final Object entity) {
+		if (map.get(entity) != null) {
+			if (mappedToSets) {
+				return ((Set<?>) map.get(entity)).toArray();
+			} else {
 				return new Object[]{map.get(entity)};
 			}
-		}else{
+		} else {
 			return new Object[]{};
 		}
 	}
 	
 	/**
-	 * Returns union of map keys and values; if all values are sets, the individual set elements are included
+	 * Returns union of map keys and values; if all values are {@link Set}s, the individual set elements are included.
 	 * @return Union of map keys and values
 	 */
-	public Set<Object> getNodes(){
-		if (mappedToSets){
+	public final Set<Object> getNodes() {
+		if (mappedToSets) {
 			Set<Object> result = Sets.newHashSet(map.keySet());
-			for (Object value : map.values()){
+			for (Object value : map.values()) {
 				result = Sets.union(result, (Set<?>) value);
 			}
 			return result;
-		}else{
+		} else {
 			return Sets.union(map.keySet(), Sets.newHashSet(map.values()));			
 		}
 	}

f1a6dcc4429f2f3d5949747d319c19067ca41daa
Todd Schiller <todd.schiller@gmail.com>
2013-10-26 17:47:50 -0700

    Clean up some capabilities: use ITask instead of AbstractTask

--- edu.washington.cs.cupid.mylyn/src/edu/washington/cs/cupid/mylyn/TaskContextCapability.java
+++ edu.washington.cs.cupid.mylyn/src/edu/washington/cs/cupid/mylyn/TaskContextCapability.java
@@ -1,6 +1,5 @@
 package edu.washington.cs.cupid.mylyn;
 
-import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
@@ -11,7 +10,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.mylyn.context.core.IInteractionContext;
 import org.eclipse.mylyn.context.core.IInteractionElement;
 import org.eclipse.mylyn.internal.context.core.ContextCorePlugin;
-import org.eclipse.mylyn.internal.tasks.core.AbstractTask;
+import org.eclipse.mylyn.tasks.core.ITask;
 import org.eclipse.mylyn.tasks.ui.TasksUi;
 
 import com.google.common.collect.Lists;
@@ -26,17 +25,13 @@ import edu.washington.cs.cupid.capability.ICapabilityArguments;
 import edu.washington.cs.cupid.capability.OptionalParameter;
 import edu.washington.cs.cupid.capability.Output;
 import edu.washington.cs.cupid.capability.Parameter;
-import edu.washington.cs.cupid.capability.ICapability.IParameter;
-import edu.washington.cs.cupid.capability.linear.LinearCapability;
-import edu.washington.cs.cupid.capability.linear.LinearJob;
-import edu.washington.cs.cupid.capability.linear.LinearStatus;
 
 /**
  * @author Todd Schiller
  */
 public class TaskContextCapability extends AbstractBaseCapability{
 
-	public static final IParameter<AbstractTask> TASK = new Parameter<AbstractTask>("Task", new TypeToken<AbstractTask>(){});
+	public static final IParameter<ITask> TASK = new Parameter<ITask>("Task", new TypeToken<ITask>(){});
 	public static final IParameter<Boolean> INCLUDE_FOLDERS = new OptionalParameter<Boolean>("Include Folders", Boolean.class, false);
 	public static final IParameter<Boolean> INCLUDE_FILES = new OptionalParameter<Boolean>("Include Files", Boolean.class, true);
 	public static final Output<List<IResource>> CONTEXT = new Output<List<IResource>>("Context", new TypeToken<List<IResource>>(){});
@@ -57,7 +52,7 @@ public class TaskContextCapability extends AbstractBaseCapability{
 				try {
 					monitor.beginTask(getName(), 100);
 				
-					AbstractTask task = input.getValueArgument(TASK);
+					ITask task = input.getValueArgument(TASK);
 					boolean includeFolders = input.getValueArgument(INCLUDE_FOLDERS);
 					boolean includeFiles = input.getValueArgument(INCLUDE_FILES);
 					
@@ -99,7 +94,7 @@ public class TaskContextCapability extends AbstractBaseCapability{
 	}
 
 
-	private boolean isActiveTask(final AbstractTask task) {
+	private boolean isActiveTask(final ITask task) {
 		return task.equals(TasksUi.getTaskActivityManager().getActiveTask());
 	}
 }
